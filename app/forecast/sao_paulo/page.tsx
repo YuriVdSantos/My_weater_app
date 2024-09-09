b9f9e8503a88ec3f3583b658424b0f1d771e2845
Yuri Vieira dos Santos <yurivieiradossantos@Yuris-Laptop.local>
2024-09-09 15:00:19 -0300

    Corrigindo forecast

--- app/forecast/sao_paulo/page.tsx
+++ app/forecast/sao_paulo/page.tsx
@@ -1,16 +1,19 @@
-// app/forecast/[cityName]/page.tsx
-import { useRouter } from 'next/router';
+'use client';
+
+import { useRouter } from 'next/navigation'; // Atualize para usar `next/navigation` para navegação
 import { useEffect, useState } from 'react';
 import PrivateRoute from '../../components/PrivateRoute'; // Ajuste o caminho se necessário
 
 const ForecastPage = () => {
   const router = useRouter();
-  const { cityName } = router.query;
   const [forecastData, setForecastData] = useState<any>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const API_KEY = 'SUA_API_KEY_AQUI'; // Insira sua chave da API do OpenWeatherMap
+  // Access cityName from URL query params
+  const cityName = router.query.cityName as string;
+
+  const API_KEY = '69125c609e5cab6c35bacd52b0ceb95b'; // Insira sua chave da API do OpenWeatherMap
 
   useEffect(() => {
     if (cityName) {
@@ -40,14 +43,16 @@ const ForecastPage = () => {
   }, [cityName]);
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Previsão para {cityName}</h1>
-      {loading && <p>Carregando...</p>}
-      {error && <p className="text-red-500">{error}</p>}
+    <div className="p-4 bg-white dark:bg-gray-800 shadow-md rounded-lg">
+      <h1 className="text-2xl font-bold mb-4 text-center text-blue-600 dark:text-white">
+        Previsão para {cityName}
+      </h1>
+      {loading && <p className="text-center">Carregando...</p>}
+      {error && <p className="text-red-500 text-center">{error}</p>}
       {forecastData && (
         <div>
           {forecastData.list.map((item: any, index: number) => (
-            <div key={index} className="mt-4 p-4 bg-gray-100 rounded">
+            <div key={index} className="mt-4 p-4 bg-gray-100 dark:bg-gray-700 rounded">
               <p>Data: {item.dt_txt}</p>
               <p>Temperatura: {item.main.temp} °C</p>
               <p>Condição: {item.weather[0].description}</p>
